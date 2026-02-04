6946a1e78d4b1c4d5e7912e98083f357dd5db59a
Sasha <khodinaal@yandex.by>
2026-02-04 13:01:13 +0300

    шаг 1.1 сделан

--- src/components/table.js
+++ src/components/table.js
@@ -17,7 +17,17 @@ export function initTable(settings, onAction) {
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+            const row = cloneTemplate(rowTemplate);
+
+            Object.keys(item).forEach(key => { 
+                if (row.elements[key]) {
+                    const element = row.elements[key];
+                    element.textContent = item[key];
+                }
+            });
+            return row.container;
+        });
         root.elements.rows.replaceChildren(...nextRows);
     }
 
